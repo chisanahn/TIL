7b0f30611ff78ec6d56a958ec5d709ff50bc2d77
Chisan Ahn <chisan2001@naver.com>
2021-09-03 17:40:51 +0900

    solved.ac class 3+ & codefoces contest 복습

--- Algorithm/BOJ/BOJ_18870.cpp
+++ Algorithm/BOJ/BOJ_18870.cpp
@@ -19,23 +19,18 @@ int main() {
     int n;
     cin >> n;
     vector<int> arr;
-    vector<int> ret;
+    map<int, int> M;
     for (int i = 0; i < n; i++) {
         int input;
         cin >> input;
         arr.push_back(input);
-        if (i == 0) {
-            ret.push_back(0);
-        }
-        else {
-            int count = 0;
-            for(int j=0; j<i; j++) {
-                if(input < arr[j]) ret[j]++;
-                if(input > arr[j]) count++;
-            }
-            ret.push_back(count);
-        }
+        M[input] = 1;
     }
-    for(auto& r : ret) cout << r << " ";
+    int index = 0;
+    for(auto& m : M) {
+        m.second = index++; 
+    }
+
+    for(auto& a : arr) cout << M[a] << " ";
     cout << '\n';
 }
--- Algorithm/BOJ/D_Make_a_Power_of_Two.cpp
+++ Algorithm/BOJ/D_Make_a_Power_of_Two.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include <algorithm>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
 #include <string>
 #include <vector>
-#include <map>
-#include <stack>
-#include <queue>
-#include <deque>
 
 using namespace std;
 using ll = long long;
@@ -16,5 +16,35 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    
+    vector<string> V;
+    ll num = 1;
+    while (num < 1000000000000000000) {
+        string s;
+        ll div = num;
+        while (div > 0) {
+            s.push_back('0' + div % 10);
+            div /= 10;
+        }
+        reverse(s.begin(), s.end());
+        V.push_back(s);
+        num *= 2;
+    }
+
+    int T;
+    cin >> T;
+    while (T--) {
+        string S;
+        cin >> S;
+        int ret = 123456789;
+        for (auto& pw2 : V) {
+            if (pw2.size() > S.size()*2) break;
+            int count = 0;
+            for (auto& s : S) {
+                if (pw2.size() == count) break;
+                if (pw2[count] == s) count++;
+            }
+            ret = min(ret, (int)(pw2.size() - count + S.size() - count));
+        }
+        cout << ret << '\n';
+    }
 }
