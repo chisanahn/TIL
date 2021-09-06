f0e2a48bb08cf46074f743bccb1478dd7ac8743f
Chisan Ahn <chisan2001@naver.com>
2021-09-06 22:06:45 +0900

    JAVA 6장 공부중

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,8 +1,18 @@
 import java.util.*;
 
+class A {
+    int a;
+}
+
+class B : public A {
+    int b;
+}
+
 class Main {
-    public static void main(String[] ars) {
-        String name = "chisan";
-        System.out.print(name.charAt(0));
+    public static void main(String[] args) {
+        A temp = new A();
+        temp.a = 10;
+        A temp = new B();
+
     }
 }
