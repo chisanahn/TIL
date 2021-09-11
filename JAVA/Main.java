db8d70893de3d370790c1bb7679b3d1f34e95174
Chisan Ahn <chisan2001@naver.com>
2021-09-11 19:55:01 +0900

    JAVA 7장 공부중

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,18 +1,62 @@
-class Tv {
-    boolean power;
-    int channel;
+import java.util.*;
 
-    void power() {
-        power = !power;
-    }
-    void channelUp() {
-        channel++;
-    }
-    void channelDown() {
-        channel--;
-    }
-}
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-class CaptionTv extends Tv {
-    boolean caption;
+        String s = scanner.nextLine();
+        char[] str = s.toCharArray();
+
+        int ret = 0;
+
+        for (char c : str) {
+            ret += 1;
+            switch (c) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    ret += 2;
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    ret += 3;
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    ret += 4;
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    ret += 5;
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    ret += 6;
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    ret += 7;
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    ret += 8;
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    ret += 9;
+                    break;
+            }
+        }
+
+        System.out.println(ret);
+    }
 }
---
+++ JAVA/Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("가위바위보 게임입니다. 가위 바위, 보 중에서 입력하세요.");
+        System.out.println("철수 >> ");
+        String insert = s.nextLine();
+        System.out.println(insert + "를 입력하셨습니다.");
+    }    
+}
