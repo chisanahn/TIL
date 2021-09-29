a66ca1552991d3a873461379ee06e0c51d86995a
Chisan Ahn <chisan2001@naver.com>
2021-09-29 15:29:59 +0900

    ICPC 특강 복습 - 세그먼트트리 구현

--- Algorithm/ICPC/BOJ_2042.cpp
+++ Algorithm/ICPC/BOJ_2042.cpp
@@ -1,57 +1,61 @@
-#include <algorithm>
-#include <deque>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <stack>
-#include <string>
 #include <vector>
 
 using namespace std;
 using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<long, long>;
 
-struct seg {
-    int n;
-    vector<long long> S;
-    long long e = 0;
-    seg(const vector<long long>& A) {
-        n = A.size();
-        S.resize(4 * n);
-        init(A, 0, n - 1, 1);
-    }
-    long long init(const vector<long long>& A, int nodeleft, int noderight, int node) {
-        if (nodeleft == noderight) {
-            return S[node] = A[nodeleft];
+class SegTree {
+    int start;  // 맨 아래층 시작점이자 맨 아래층 길이, S[start+i]= A[i]
+    vector<ll> S;
+    int a_size;  // A.size()
+   public:
+    SegTree(vector<ll>& A) : S(vector<ll>(A.size() * 4, 0)), a_size(A.size()) {
+        start = 1;
+        while (start < A.size()) start *= 2;
+        // 맨 아래층 원소들 업데이트
+        for (int i = 0; i < A.size(); i++) {
+            S[i + start] = A[i];
         }
-        int mid = (nodeleft + noderight) / 2;
-        return S[node] = init(A, nodeleft, mid, node * 2) + init(A, mid + 1, noderight, node * 2 + 1);
-    }
-
-    long long query(int left, int right, int node, int nodeleft, int noderight) {
-        if (right < nodeleft || noderight < left) return e;
-        if (left <= nodeleft && noderight <= right) return S[node];
-
-        int mid = (nodeleft + noderight) / 2;
-        return query(left, right, node * 2, nodeleft, mid) + query(left, right, node * 2 + 1, mid + 1, noderight);
-    }
-    long long query(int left, int right) {
-        return query(left, right, 1, 0, n - 1);
-    }
-    void update(int index, long long newvalue, int node, int nodeleft, int noderight) {
-        if (index < nodeleft || noderight < index) return;
-        if (nodeleft == noderight) {
-            S[node] = newvalue;
-            return;
+        // 다른 요소들 업데이트
+        int index = start;
+        while (index > 1) {
+            index--;
+            S[index] = S[index * 2] + S[index * 2 + 1];
         }
-        int mid = (nodeleft + noderight) / 2;
-        update(index, newvalue, node * 2, nodeleft, mid);
-        update(index, newvalue, node * 2 + 1, mid + 1, noderight);
-        S[node] = S[node * 2] + S[node * 2 + 1];
     }
-    void update(int index, long long newvalue) {
-        update(index, newvalue, 1, 0, n - 1);
+    // A[n]을 value값으로 업데이트
+
+    void update(int n, ll value) {
+        // 맨 아래층 업데이트
+        int index = start + n;
+        S[index] = value;
+        // 관련 원소들 업데이트
+        while (index > 1) {
+            index /= 2;
+            S[index] = S[index * 2] + S[index * 2 + 1];
+        }
+    }
+    // A[left] ~ A[right]의 구간 합
+    ll ret(int left, int right, int S_index, int L, int R) {
+        // 범위에 맞는 경우
+        if (left == L && right == R)
+            return S[S_index];
+        // 범위가 더 작은 경우 재귀처리
+        else {
+            int mid = (L + R) / 2;
+            if (mid >= right)
+                return ret(left, right, S_index * 2, L, mid);
+            else if (mid + 1 <= left)
+                return ret(left, right, S_index * 2 + 1, mid + 1, R);
+            else
+                return ret(left, mid, S_index * 2, L, mid) + ret(mid + 1, right, S_index * 2 + 1, mid + 1, R);
+        }
+    }
+    // 인터페이스
+    ll ret(int left, int right) {
+        // 예외 처리
+        if(left < 0 || right >= a_size) return 0;
+        return ret(left, right, 1, 0, start - 1);
     }
 };
 
@@ -61,12 +65,10 @@ int main() {
 
     int n, m, k;
     cin >> n >> m >> k;
-    vector<ll> arr(n);
-    for (auto& a : arr) cin >> a;
-
-    seg S(arr);
-
-    for (int i = 0; i < m + k; i++) {
+    vector<ll> A(n);
+    for (auto& a : A) cin >> a;
+    SegTree ST(A);
+    for (int i = 0; i < k + m; i++) {
         int cmd;
         cin >> cmd;
         if (cmd == 1) {
@@ -74,14 +76,13 @@ int main() {
             ll c;
             cin >> b >> c;
             b--;
-            S.update(b, c);
-        }
-        if (cmd == 2) {
+            ST.update(b, c);
+        } else {
             int b, c;
             cin >> b >> c;
             b--;
             c--;
-            cout << S.query(b, c) << '\n';
+            cout << ST.ret(b, c) << '\n';
         }
     }
 }
