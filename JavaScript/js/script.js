5e867c58096a07412eca23abab168c40fb93b2fc
Chisan Ahn <chisan2001@gmail.com>
2022-01-22 11:38:03 +0900

    2022-01-21

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -1,2 +1,11 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+let button = document.getElementById("changeBackground");
+
+let count = 0;
+let color;
+button.addEventListener("click", () => {
+	count++;
+	count %= 2;
+	if (count == 0) color = "white";
+	else color = "fuchsia";
+	document.body.style.backgroundColor = color;
+});
